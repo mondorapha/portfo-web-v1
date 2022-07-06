a41a9b28fa3a851941cba83e8ac830dd2ea854ec
mondorapha <raphaestevam@gmail.com>
2022-07-06 14:13:35 -0300

    fixed unnecessary function in .py file

--- webserver.py
+++ webserver.py
@@ -15,30 +15,6 @@ def my_portfolio_page(page_name):
     return render_template(page_name)
 
 
-def write_to_form_database(data):
-    try:
-        with open('./database.txt', mode='r', encoding='UTF-8') as form_database:
-            form_database_content = form_database.read()
-            with open('./database.txt', mode='a', encoding='UTF-8') as form_database_to_append:
-                email = data['email']
-                subject = data['subject']
-                message = data['message']
-                if os.path.getsize('./database.txt') > 0:
-                    if not form_database_content.endswith('\n'):
-                        form_database_to_append.write('\n')
-                    form_database_to_append.write(
-                        f'{email},{subject},{message}')
-                else:
-                    form_database_to_append.write(
-                        f'{email},{subject},{message}')
-    except FileNotFoundError as err1:
-        print('Ooops, something wrong. Database file not found.')
-        raise err1
-    except IOError as err2:
-        print('Ooops, something went wrong. IO error.')
-        raise err2
-
-
 def write_to_csv_database(data):
     try:
         with open('./database.csv', mode='a', newline='', encoding='UTF-8') as csv_database_to_append:
